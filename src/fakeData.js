8c539900ef316709c3708ee5b3b01a0a1c110ea9
“T3sultan” <incognito.soul011@gmail.com>
2021-09-26 00:09:39 +0600

    data

---
+++ src/fakeData.js
@@ -0,0 +1,149 @@
+const programmerData = [
+    {
+        id: 1,
+        name: 'David Patterson',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/David_A_Patterson.jpg/220px-David_A_Patterson.jpg',
+        prefession: 'Distinguished Engineer',
+        born: 'November 16, 1947 (age 73)',
+        citizenship: 'United States',
+        education: 'University of California',
+        awards: 'Turing Award (2017)',
+        salary: 1000,
+        fields: 'Computer science',
+        website: 'www2.eecs.berkeley.edu/Faculty/Homepages/patterson.html'
+    },
+    {
+        id: 2,
+        name: 'John Carmack',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/John_Carmack_at_GDCA_2017_--_1_March_2017_%28cropped%29.jpeg/220px-John_Carmack_at_GDCA_2017_--_1_March_2017_%28cropped%29.jpeg',
+        prefession: 'Game Developers Choice Awards',
+        born: 'August 20, 1970 (age 51',
+        citizenship: 'American',
+        education: 'University of Missouri, Kansas City',
+        awards: 'Quake 3 engine',
+        salary: 1200,
+        fields: 'computer programmer',
+        website: 'https://en.wikipedia.org/wiki/John_Carmack',
+    },
+
+    {
+        id: 3,
+        name: 'Donald Knuth',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/KnuthAtOpenContentAlliance.jpg/192px-KnuthAtOpenContentAlliance.jpg',
+        prefession: 'computer scientist, mathematician',
+        born: 'January 10, 1938 (age 83)',
+        citizenship: 'American',
+        education: 'Case Institute of Technology (B.S., M.S.)',
+        awards: 'SIGCSE Outstanding Contribution (1986)',
+        salary: 800,
+        fields: 'Mathematics Computer science',
+        website: 'cs.stanford.edu/~knuth'
+    },
+
+    {
+        id: 4,
+        name: 'Guido van Rossum',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Guido-portrait-2014-drc.jpg/220px-Guido-portrait-2014-drc.jpg',
+        prefession: 'Python programming language',
+        born: '31 January 1956 (age 65)',
+        citizenship: 'Dutch',
+        education: 'University of Amsterdam',
+        awards: 'Advancement of Free Software (2001)',
+        salary: 1400,
+        fields: 'mathematics and Computer science',
+        website: 'gvanrossum.github.io'
+    },
+
+    {
+        id: 5,
+        name: 'James Gosling',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/James_Gosling_2008.jpg/220px-James_Gosling_2008.jpg',
+
+        prefession: 'Java programming language',
+        born: 'May 19, 1955 (age 66)',
+        citizenship: 'Canadian',
+        education: 'University of Calgary',
+        awards: 'IEEE John von Neumann Medal The Economist Innovation Award NAE Foreign Member',
+        salary: 7000,
+        fields: 'computer scientist',
+        website: 'www2.eecs.berkeley.edu/Faculty/Homepages/patterson.html'
+    },
+
+    {
+        id: 6,
+        name: 'Niklaus Emil Wirth',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Niklaus_Wirth%2C_UrGU.jpg/220px-Niklaus_Wirth%2C_UrGU.jpg',
+
+        prefession: 'programming languages, including Pascal',
+        born: '15 February 1934 (age 87)',
+        citizenship: 'Switzerland',
+        education: 'University of California, Berkeley',
+        awards: 'IEEE Emanuel R. Piore Award',
+        salary: 1500,
+        fields: 'Computer science',
+        website: 'www2.eecs.berkeley.edu/Faculty/Homepages/patterson.html'
+    },
+
+    {
+        id: 7,
+        name: 'Alan Kay',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Alan_Kay_%283097597186%29_%28cropped%29.jpg/220px-Alan_Kay_%283097597186%29_%28cropped%29.jpg',
+
+        prefession: 'object-oriented programming ',
+        born: 'object-oriented programming ',
+        citizenship: 'United States',
+        education: 'University of Colorado at Boulde',
+        awards: 'ACM Turing Award',
+        salary: 1600,
+        fields: 'Computer science',
+        website: 'www2.eecs.berkeley.edu/Faculty/Homepages/patterson.html'
+    },
+
+    {
+        id: 8,
+        name: 'John Patrick McCarthy',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/John_McCarthy_Stanford.jpg/220px-John_McCarthy_Stanford.jpg',
+
+        prefession: 'Artificial intelligence',
+        born: 'September 4, 1927',
+        citizenship: 'United States',
+        education: 'Princeton University, California Institute of Technology',
+        awards: 'Computer Pioneer Award (1985)',
+        salary: 1700,
+        fields: 'Computer science',
+        website: 'www2.eecs.berkeley.edu/Faculty/Homepages/patterson.html'
+    },
+
+    {
+        id: 9,
+        name: 'John George Kemeny',
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/13/John_George_Kemeny.jpg/220px-John_George_Kemeny.jpg',
+
+        prefession: 'BASIC programming language ',
+        born: '	May 31, 1926',
+        citizenship: 'Hungary',
+        education: 'Princeton University (A.B., Ph.D.)',
+        awards: 'Computer Pioneer Award',
+        salary: 800,
+        fields: 'mathematician, computer scientist',
+        website: 'www2.eecs.berkeley.edu/Faculty/Homepages/patterson.html'
+    },
+
+    {
+        id: 10,
+        name: 'Grace Murray Hopper ',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Commodore_Grace_M._Hopper%2C_USN_%28covered%29.jpg/220px-Commodore_Grace_M._Hopper%2C_USN_%28covered%29.jpg',
+
+        prefession: 'high-level programming language',
+        born: 'December 9, 1906',
+        citizenship: 'United States of America',
+        education: 'Vassar College (BA)',
+        awards: 'efense Distinguished Service Medal',
+        salary: 700,
+        fields: 'computer scientist',
+        website: 'www2.eecs.berkeley.edu/Faculty/Homepages/patterson.html'
+    },
+
+
+
+]
