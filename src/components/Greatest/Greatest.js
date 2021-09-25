2334f0e77186dc6e5bc813e54abe444280294de6
“T3sultan” <incognito.soul011@gmail.com>
2021-09-26 00:27:20 +0600

    header component

---
+++ src/components/Greatest/Greatest.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Greatest.css'
+
+const Greatest = () => {
+    return (
+        <div>
+        <h1>Greatest</h1>
+            
+        </div>
+    );
+};
+
+export default Greatest;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="headerTitle">
+            <h1 className="textStyle">Greatest Computer Programmers Of All Time</h1>
+            <p className="dollerTitleStyle">Every Programmer Average Yearly Salary 200 Million</p>
+        </div>
+    );
+};
+
+export default Header;
